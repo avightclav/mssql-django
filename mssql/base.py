39cbdf63413c3c830ba486b49e6f2d683d4875e2
avightclav <Dmitriy.Klabukov@macrogroup.ru>
2025-02-02 01:15:49 +0300

    change internal can_return_rows_from_bulk_insert flag in case user have multiple connections defined

--- mssql/base.py
+++ mssql/base.py
@@ -431,6 +431,8 @@ class DatabaseWrapper(BaseDatabaseWrapper):
         # inserting into tables with triggers causes errors. See issue #130
         if (options.get('return_rows_bulk_insert', False)):
             self.features_class.can_return_rows_from_bulk_insert = True
+        else:
+            self.features_class.can_return_rows_from_bulk_insert = False
 
         val = self.get_system_datetime
         if isinstance(val, str):
